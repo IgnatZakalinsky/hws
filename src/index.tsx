ffeb1c3809093bd17fc9c3eb9994c5176423f51d
neko <ai73a@yandex.ru>
2022-08-29 09:12:00 +0300

    test h1 1 without friend message

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
+import App from './s1-main/App'
 import reportWebVitals from './reportWebVitals'
+// import { Provider } from 'react-redux'
+// import store from './s2-homeworks/hw10/bll/store'
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
+        {/*для дз 10*/}
+        {/*<Provider store={store}>*/}
         <App/>
+        {/*</Provider>*/}
     </React.StrictMode>
 )
 
---
+++ src/s1-main/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import s from './App.module.css'
+import HW1 from '../s2-homeworks/hw01/HW1'
+
+function App() {
+    // для дз 12
+    // const themeId = useSelector((state: any) => state.theme.themeId)
+    // useEffect(() => {
+    //     document.documentElement.dataset.theme = themeId
+    // }, [themeId])
+
+    return (
+        <div
+            className={s.App}
+            // data-theme={themeId} // для дз 12
+        >
+            <HW1/>
+            {/*<HW2/>*/}
+            {/*<HW3/>*/}
+            {/*<HW4/>*/}
+
+            {/*<HW5/>*/}
+        </div>
+    )
+}
+
+export default App
