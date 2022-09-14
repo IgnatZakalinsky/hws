96540a81eda8bd94cb62c7347618583ff757aa12
neko <ai73a@yandex.ru>
2022-09-14 13:57:31 +0300

    add hw2

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import s from './App.module.css'
 import HW1 from '../s2-homeworks/hw01/HW1'
+import HW2 from '../s2-homeworks/hw02/HW2'
 
 function App() {
     // для дз 12
@@ -15,7 +16,7 @@ function App() {
             // data-theme={themeId} // для дз 12
         >
             <HW1/>
-            {/*<HW2/>*/}
+            <HW2/>
             {/*<HW3/>*/}
             {/*<HW4/>*/}
 
